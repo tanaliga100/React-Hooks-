83cbd06a4efe671ed6b916320bc0cc72c6e2d4e1
Jordan <jordantanaliga@gmail.com>
2022-02-18 06:25:22 +0800

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+const [counter, setCounter]  = useState(0)
+const  [inputValue, setInputValue] =useState('')
+
+const increment = () => {
+  setCounter(counter + 10)
+}
+
+const inputHandler = (e) => {
+  const newvalue = e.target.value;
+  setInputValue(newvalue)
+  console.log(inputValue);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">HELLO useState-Hook</h1>
+      <section>
+        <span>{counter}</span>
+        <button onClick={increment}>Increment</button>
+      </section>
+
+      <section>
+        <input  type="text" placeholder='type something' value={inputValue} onChange={inputHandler} />
+        {inputValue}
+      </section>
     </div>
   );
 }
