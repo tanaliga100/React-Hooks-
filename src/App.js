e724fadae0865812d2eb2e3ff3b4ede48457f7d6
Jordan <jordantanaliga@gmail.com>
2022-02-18 06:36:25 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -3,30 +3,19 @@ import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
-const [counter, setCounter]  = useState(0)
-const  [inputValue, setInputValue] =useState('')
 
-const increment = () => {
-  setCounter(counter + 10)
-}
 
-const inputHandler = (e) => {
-  const newvalue = e.target.value;
-  setInputValue(newvalue)
-  console.log(inputValue);
-}
+
 
   return (
     <div className="App">
-      <h1 className="title">HELLO useState-Hook</h1>
+      <h1 className="title">HELLO useReducer-Hook</h1>
       <section>
-        <span>{counter}</span>
-        <button onClick={increment}>Increment</button>
+
       </section>
 
       <section>
-        <input  type="text" placeholder='type something' value={inputValue} onChange={inputHandler} />
-        {inputValue}
+
       </section>
     </div>
   );
