5c2c3c2e483fb6e842dae7f022ab31e452cf5751
Jordan <jordantanaliga@gmail.com>
2022-02-18 06:49:56 +0800

    final

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
-import { useRef, useState } from 'react';
-import './App.css';
+import logo from "./logo.svg";
+import { useReducer, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const initialState = {
+    counter: 0,
+    showText: false,
+  };
 
+  const functionReducer = (state, action) => {
+    switch (action.type) {
+      case "increment":
+        return { counter: state.counter + 1, showText: state.showText };
+        break;
+      case "toggle":
+        return { counter: state.counter, showText: !state.showText };
+      default:
+        return state;
+        break;
+    }
+  };
 
-
+  const [state, dispatch] = useReducer(functionReducer, initialState);
 
   return (
     <div className="App">
       <h1 className="title">HELLO useReducer-Hook</h1>
       <section>
-
+        <h1>{state.counter}</h1>
+        <button
+          onClick={() => {
+            dispatch({ type: "increment" });
+            dispatch({ type: "toggle" });
+          }}
+        >
+          Click
+        </button>
       </section>
-
-      <section>
-
-      </section>
+      <section>{state.showText && <p>This is a text</p>}</section>
     </div>
   );
 }
