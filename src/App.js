43053404e4b8133e5f09e28c395661f9c6c67cb4
Jordan <jordantanaliga@gmail.com>
2022-02-18 08:01:53 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
 import logo from "./logo.svg";
-import { useEffect, useReducer, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useReducer, useRef, useState } from "react";
 import "./App.css";
 
-
-  const initialState = {
-    name: null,
-    age: 0,
-  }
-
 function App() {
-  const [data, setData] = useState(initialState)
-  const inputRef = useRef(null)
+  useLayoutEffect(() => {
+    console.log('layout');
+  })
+  useEffect(()=> {
+    console.log('effect');
+  })
   
-  const clickHandler = (e) => {
-    e.preventDefault()
-    setData({
-      name: inputRef.current.value,
-    })
-    inputRef.current.value = ""
-    inputRef.current.focus()
-  }
-
   return (
     <div className="App">
-      <h1 className="title">HELLO useRef-Hook</h1>
+      <h1 className="title">HELLO useLayOutEffect-Hook</h1>
       <section>
-      <form action=""  onSubmit={clickHandler}>
-      <input ref={inputRef} type="text" placeholder="Ex..." />
-      </form>
       </section>
-      <h1>{data.name}</h1>
       <section>
-      <button onClick={clickHandler}>Clear</button>
       </section>
     </div>
   );
