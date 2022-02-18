b72445d0cc83bb0f26ffede0eb06adafdbd55071
Jordan <jordantanaliga@gmail.com>
2022-02-18 08:44:32 +0800

    final commit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import { useEffect, useLayoutEffect, useReducer, useRef, useState } from "react"
 import "./App.css";
 
 function App() {
+  
+  const inputRef = useRef(null)
+
   useLayoutEffect(() => {
-    console.log('layout');
-  })
+    console.log('layout', inputRef.current.value);
+  },[])
+
   useEffect(()=> {
-    console.log('effect');
-  })
+    console.log('effect', inputRef.current.value = "Hello");
+  },[])
+
   
   return (
     <div className="App">
       <h1 className="title">HELLO useLayOutEffect-Hook</h1>
       <section>
+        <input ref={inputRef} value="Jords" type="text" readOnly />
       </section>
       <section>
       </section>
